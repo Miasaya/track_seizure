7a81517f1048cba710baab18b474333db31f4852
Mailine <nguyen.mailine@gmail.com>
2020-08-04 10:00:38 +0200

    Added bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/homescreen.dart';
+import 'package:flutter/services.dart';
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
   runApp(MyApp());
-}
-
+  }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+import 'package:track_seizure/component/bottomBar.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Track your Seizures',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: <Widget>[
+            Container(
+                child: Center(child: Text('This is my seizure tracking app'))),
+          ],
         ),
-        home: Scaffold(
-          backgroundColor: Colors.white10,
-          body: Container(
-              child: Center(child: Text('This is my seizure tracking app'))),
-        ));
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_view_day),
+            title: Text('Log'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart),
+            title: Text('Statistics'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    ));
   }
 }
