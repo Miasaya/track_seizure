842444eed2c20984ec1434edf3e068c6ce6cc06b
Mailine <nguyen.mailine@gmail.com>
2020-08-04 14:23:48 +0200

    Added track button

--- lib/component/constants.dart
+++ lib/component/constants.dart
@@ -4,11 +4,18 @@ const TextStyle trackHeaderStyle = TextStyle(
   fontSize: 30, 
   fontWeight: FontWeight.bold,
   fontFamily: 'Open',
-  color: Color(0xFF252726),
+  color: Color(0xFF707070),
   );
 
 const TextStyle kHeaderTextStyle = TextStyle(
   fontSize: 28, 
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Open',
+  color: Colors.white,
+  );
+
+const TextStyle kTrackingButton = TextStyle(
+  fontSize: 18, 
   fontWeight: FontWeight.bold,
   fontFamily: 'Open',
   color: Colors.white,
--- lib/screens/tracking_page.dart
+++ lib/screens/tracking_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 import 'package:track_seizure/component/constants.dart';
 import 'package:track_seizure/component/header.dart';
 
@@ -18,14 +17,45 @@ class _TrackingPageState extends State<TrackingPage> {
     return Column(
       children: <Widget>[
         Header(),
-        Center(
-          child: Text(
-            'Anything up today?',
-            style: trackHeaderStyle,
+        Text(
+          'Anything up today?',
+          style: trackHeaderStyle,
+        ),
+        SizedBox(
+          height: 40.0,
+        ),
+        Container(
+          height: 50.0,
+          child: RaisedButton(
+            onPressed: () {},
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(80.0)),
+            padding: EdgeInsets.all(0.0),
+            child: Ink(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF554BD8),
+                      Color(0xFF804CD9),
+                    ],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0)),
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                alignment: Alignment.center,
+                child: Text(
+                  "Track",
+                  textAlign: TextAlign.center,
+                  style: kTrackingButton,
+                ),
+              ),
+            ),
           ),
-        )
+        ),
+        SizedBox(height: 40.0,),
       ],
     );
   }
 }
-
