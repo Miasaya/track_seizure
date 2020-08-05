fbcb5833bef7f9b78491f4c96b1f868071636c74
Mailine <nguyen.mailine@gmail.com>
2020-08-05 17:20:21 +0200

    Screen overlap correction

--- lib/screens/new_entry.dart
+++ lib/screens/new_entry.dart
@@ -23,147 +23,153 @@ class _NewEntryPageState extends State<NewEntryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            ClipPath(
-                clipper: ClipperEntry(),
-                child: Container(
-                  width: double.infinity,
-                  height: 200.0,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          end: Alignment.topLeft,
-                          colors: [
-                        kBottomGradientColor,
-                        kTopGradientColor,
-                      ])),
-                )),
-            Text(
-              'Which type of seizure ?',
-              style: trackHeaderStyle,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                EntryOption(
-                  colour: Colors.orange[300],
-                  text: 'Absence',
-                  icon: FontAwesome.eye,
-                  onPress: () {
-                    setState(() {
-                      selectedType = Type.absence;
-                    });
-                    print(selectedType);
-                  },
-                  textstyle: selectedType == Type.absence
-                      ? kNewEntryTextStyleActive
-                      : kNewEntryTextStyleInactive,
-                ),
-                EntryOption(
-                  colour: Colors.red[400],
-                  text: 'Generalized',
-                  icon: FontAwesome.male,
-                  onPress: () {
-                    setState(() {
-                      selectedType = Type.tc;
-                    });
-                    print(selectedType);
-                  },
-                  textstyle: selectedType == Type.tc
-                      ? kNewEntryTextStyleActive
-                      : kNewEntryTextStyleInactive,
-                ),
-                EntryOption(
-                  colour: Colors.green[400],
-                  text: 'Other',
-                  icon: FontAwesome.question,
-                  onPress: () {
-                    setState(() {
-                      selectedType = Type.other;
-                    });
-                    print(selectedType);
-                  },
-                  textstyle: selectedType == Type.other
-                      ? kNewEntryTextStyleActive
-                      : kNewEntryTextStyleInactive,
-                ),
-              ],
-            ),
-            SizedBox(height: 15.0),
-            Text(
-              'Lenght : $lenght sec',
-              style: trackHeaderStyle,
-            ),
-            SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  inactiveTrackColor: Color(0xFFd9d9d9),
-                  activeTrackColor: kBottomGradientColor,
-                  thumbColor: kBottomGradientColor,
-                  overlayColor: Color(0x295126AB),
-                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10.0),
-                  overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                ),
-                child: Slider(
-                    value: lenght.toDouble(),
-                    min: 10.0,
-                    max: 120.0,
-                    onChanged: (double newValue) {
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ClipPath(
+                  clipper: ClipperEntry(),
+                  child: Container(
+                    width: double.infinity,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            end: Alignment.topLeft,
+                            colors: [
+                          kBottomGradientColor,
+                          kTopGradientColor,
+                        ])),
+                    child: Image.asset(
+                      'assets/images/stay.png',
+                      width: 100,
+                    ),
+                  )),
+              Text(
+                'Which type of seizure ?',
+                style: trackHeaderStyle,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  EntryOption(
+                    colour: Colors.orange[300],
+                    text: 'Absence',
+                    icon: FontAwesome.eye,
+                    onPress: () {
                       setState(() {
-                        lenght = newValue.round();
+                        selectedType = Type.absence;
                       });
-                    })),
-            SizedBox(height: 15.0),
-            Text(
-              'Intensity : $feel /10',
-              style: trackHeaderStyle,
-            ),
-            SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  inactiveTrackColor: Color(0xFFd9d9d9),
-                  activeTrackColor: kBottomGradientColor,
-                  thumbColor: kBottomGradientColor,
-                  overlayColor: Color(0x295126AB),
-                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10.0),
-                  overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                ),
-                child: Slider(
-                    value: feel.toDouble(),
-                    max: 10.0,
-                    onChanged: (double newValue) {
+                      print(selectedType);
+                    },
+                    textstyle: selectedType == Type.absence
+                        ? kNewEntryTextStyleActive
+                        : kNewEntryTextStyleInactive,
+                  ),
+                  EntryOption(
+                    colour: Colors.red[400],
+                    text: 'Generalized',
+                    icon: FontAwesome.male,
+                    onPress: () {
                       setState(() {
-                        feel = newValue.round();
+                        selectedType = Type.tc;
                       });
-                    })),
-            SizedBox(height: 15.0),
-            Text(
-              'Additional notes:',
-              style: trackHeaderStyle,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                style: kInputTextStyle,
-                controller: nameController,
-                cursorColor: Colors.white,
-                decoration: noteDecoration(),
-                onChanged: (text) {
-                  setState(() {
-                    note = text;
-                  });
+                      print(selectedType);
+                    },
+                    textstyle: selectedType == Type.tc
+                        ? kNewEntryTextStyleActive
+                        : kNewEntryTextStyleInactive,
+                  ),
+                  EntryOption(
+                    colour: Colors.green[400],
+                    text: 'Other',
+                    icon: FontAwesome.question,
+                    onPress: () {
+                      setState(() {
+                        selectedType = Type.other;
+                      });
+                      print(selectedType);
+                    },
+                    textstyle: selectedType == Type.other
+                        ? kNewEntryTextStyleActive
+                        : kNewEntryTextStyleInactive,
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              Text(
+                'Lenght : $lenght sec',
+                style: trackHeaderStyle,
+              ),
+              SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    inactiveTrackColor: Color(0xFFd9d9d9),
+                    activeTrackColor: kBottomGradientColor,
+                    thumbColor: kBottomGradientColor,
+                    overlayColor: Color(0x295126AB),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                  ),
+                  child: Slider(
+                      value: lenght.toDouble(),
+                      min: 10.0,
+                      max: 120.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          lenght = newValue.round();
+                        });
+                      })),
+              SizedBox(height: 15.0),
+              Text(
+                'Intensity : $feel /10',
+                style: trackHeaderStyle,
+              ),
+              SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    inactiveTrackColor: Color(0xFFd9d9d9),
+                    activeTrackColor: kBottomGradientColor,
+                    thumbColor: kBottomGradientColor,
+                    overlayColor: Color(0x295126AB),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                  ),
+                  child: Slider(
+                      value: feel.toDouble(),
+                      max: 10.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          feel = newValue.round();
+                        });
+                      })),
+              SizedBox(height: 15.0),
+              Text(
+                'Additional notes:',
+                style: trackHeaderStyle,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  style: kInputTextStyle,
+                  controller: nameController,
+                  cursorColor: Colors.white,
+                  decoration: noteDecoration(),
+                  onChanged: (text) {
+                    setState(() {
+                      note = text;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              EntryButton(
+                text: 'Save',
+                onPress: () {
+                  Navigator.pop(context);
                 },
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            EntryButton(
-              text: 'Save',
-              onPress: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
@@ -195,4 +201,3 @@ class ClipperEntry extends CustomClipper<Path> {
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
-
