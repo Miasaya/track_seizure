6a8f12a42143515e867739c914ced54ca456d9e5
Mailine <nguyen.mailine@gmail.com>
2020-08-05 12:00:58 +0200

    Hop Hop

--- lib/screens/new_entry.dart
+++ lib/screens/new_entry.dart
@@ -25,7 +25,20 @@ class _NewEntryPageState extends State<NewEntryPage> {
         backgroundColor: Colors.white,
         body: Column(
           children: [
-            SizedBox(height: 40.0),
+            ClipPath(
+                clipper: ClipperEntry(),
+                child: Container(
+                  width: double.infinity,
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          end: Alignment.topLeft,
+                          colors: [
+                        kBottomGradientColor,
+                        kTopGradientColor,
+                      ])),
+                )),
             Text(
               'Which type of seizure ?',
               style: trackHeaderStyle,
@@ -141,7 +154,9 @@ class _NewEntryPageState extends State<NewEntryPage> {
                 },
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             EntryButton(
               text: 'Save',
               onPress: () {
@@ -152,3 +167,32 @@ class _NewEntryPageState extends State<NewEntryPage> {
         ));
   }
 }
+
+class ClipperEntry extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, size.height - 20);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2.25, size.height - 30.0);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondControlPoint =
+        Offset(size.width - (size.width / 3.25), size.height - 65);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
